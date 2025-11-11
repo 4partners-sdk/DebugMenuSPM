d279809012b5db60f5511a83793915e45a3cbe6c
Viktor Golovach <viktorgolovach@Viktors-MacBook-Air.local>
2025-11-11 14:58:07 +0100

    update DebugMenuUserDefaultsService

--- Sources/Debug/DebugMenuUserDefaults.swift
+++ Sources/Debug/DebugMenuUserDefaults.swift
@@ -1,18 +1,18 @@
 import Foundation
 
-protocol DebugMenuUserDefaultsProviding {
-    var simulatePremium: Bool { get set }
-    var simulatePurchase: Bool { get set }
-    var simulateNegativeFlow: Bool { get set }
-    var simulateMissingAdaptyProduct: Bool { get set }
-    var simulateSplashError: Bool { get set }
-    var shouldSetSpecialOfferTimer: Bool { get set }
-    var shouldSetSecurityCenterTimer: Bool { get set }
-    var simulateOldiOSVersion: Bool { get set }
-    var emailForCheck: String { get set }
+public protocol DebugMenuUserDefaultsProviding {
+    public var simulatePremium: Bool { get set }
+    public var simulatePurchase: Bool { get set }
+    public var simulateNegativeFlow: Bool { get set }
+    public var simulateMissingAdaptyProduct: Bool { get set }
+    public var simulateSplashError: Bool { get set }
+    public var shouldSetSpecialOfferTimer: Bool { get set }
+    public var shouldSetSecurityCenterTimer: Bool { get set }
+    public var simulateOldiOSVersion: Bool { get set }
+    public var emailForCheck: String { get set }
 }
 
-enum DebugMenuUserDefaultsKey: String {
+public enum DebugMenuUserDefaultsKey: String {
     case simulatePremium
     case simulatePurchase
     case simulateNegativeFlow
@@ -28,53 +28,53 @@ enum DebugMenuUserDefaultsKey: String {
     }
 }
 
-final class DebugMenuUserDefaultsService: DebugMenuUserDefaultsProviding {
-    static let shared = DebugMenuUserDefaultsService()
+public class DebugMenuUserDefaultsService: DebugMenuUserDefaultsProviding {
+    public static let shared = DebugMenuUserDefaultsService()
     private init() {}
     
     private let defaults = UserDefaults.standard
     
-    var simulatePremium: Bool {
+    public var simulatePremium: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.simulatePremium.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.simulatePremium.raw) }
     }
     
-    var simulatePurchase: Bool {
+    public var simulatePurchase: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.simulatePurchase.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.simulatePurchase.raw) }
     }
     
-    var simulateNegativeFlow: Bool {
+    public var simulateNegativeFlow: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.simulateNegativeFlow.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.simulateNegativeFlow.raw) }
     }
     
-    var simulateMissingAdaptyProduct: Bool {
+    public var simulateMissingAdaptyProduct: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.simulateMissingAdaptyProduct.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.simulateMissingAdaptyProduct.raw) }
     }
     
-    var simulateSplashError: Bool {
+    public var simulateSplashError: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.simulateSplashError.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.simulateSplashError.raw) }
     }
     
-    var shouldSetSpecialOfferTimer: Bool {
+    public var shouldSetSpecialOfferTimer: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.shouldSetSpecialOfferTimer.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.shouldSetSpecialOfferTimer.raw) }
     }
     
-    var shouldSetSecurityCenterTimer: Bool {
+    public var shouldSetSecurityCenterTimer: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.shouldSetSecurityCenterTimer.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.shouldSetSecurityCenterTimer.raw) }
     }
     
-    var simulateOldiOSVersion: Bool {
+    public var simulateOldiOSVersion: Bool {
         get { defaults.bool(forKey: DebugMenuUserDefaultsKey.shouldSetOldiOSVersion.raw) }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.shouldSetOldiOSVersion.raw) }
     }
     
-    var emailForCheck: String {
+    public var emailForCheck: String {
         get { defaults.string(forKey: DebugMenuUserDefaultsKey.emailForCheck.raw) ?? "" }
         set { defaults.set(newValue, forKey: DebugMenuUserDefaultsKey.emailForCheck.raw) }
     }
